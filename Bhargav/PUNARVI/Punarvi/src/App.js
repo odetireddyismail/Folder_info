1cd2d156f77b6257d69d29ec4169be9fd1228561
odetireddyismail <106577334+odetireddyismail@users.noreply.github.com>
2025-02-04 18:36:27 +0530

    E:

---
+++ Bhargav/PUNARVI/Punarvi/src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+
+import React from "react";
+import "./App.css";
+import { Main } from "./Main";
+
+function App() {
+  //  const ref = useRef(null);
+
+  // const handleClick = () => {
+  //   ref.current?.scrollIntoView({ behavior: "smooth" });
+  // };
+
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
